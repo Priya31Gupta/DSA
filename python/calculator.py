2496658cade3e24676e79827bf2b00af9d14b00f
Priya Gupta <priyakumarigupta790@gmail.com>
2024-10-11 01:46:45 +0530

    Create calculator.py

---
+++ python/calculator.py
@@ -0,0 +1,27 @@
+num1 = input("Enter first number")
+a = int(num1)
+num2 = input("Enter second number")
+b=int(num2)
+
+c='s'
+print(f"c: {c}")
+
+# addition
+add=a+b
+print(f"Addition of {a} and {b} is {add}")
+
+#substraction
+sub=a-b
+print(f"Substraction of {a} and {b} is {sub}")
+
+#multiplication
+mul=a*b
+print(f"multiplication of {a} and {b} is {mul}")
+
+#division
+div=a/b
+print(f"division of {a} and {b} is {div}")
+
+#modulus
+mod=a%b
+print(f"modulus of {a} and {b} is {mod}")
